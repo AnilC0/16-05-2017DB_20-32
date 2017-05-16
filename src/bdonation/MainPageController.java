89ccb56c1f09c4965c77e341aa9e8b1630e47410
AnilC0 <cakiranil998@gmail.com>
2017-05-16 20:28:47 +0300

    scene değişmiyor YINE

---
+++ src/bdonation/MainPageController.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bdonation;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ANIL
+ */
+public class MainPageController implements Initializable {
+    
+    @FXML
+    private Label label;
+    @FXML
+    private Button rLogout,nLogout,sLogout,lLogin;
+    /*
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    */
+    
+    
+    @FXML
+    private void loginout(ActionEvent event) throws IOException {
+     Stage stage; 
+     Parent root;
+     if(event.getSource()==rLogout || event.getSource()== sLogout || event.getSource()==nLogout ){
+        stage=(Stage) rLogout.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("BloodLogin.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+     }
+     else if(event.getSource()==lLogin){
+        stage=(Stage) lLogin.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+     }
+    }
+    
+
+    
+    
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
